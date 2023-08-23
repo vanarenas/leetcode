3ba7bb77e9fecf1f035f64ec65eff1fc10b5c25d
Evangielyn Arenas <119069231+vanarenas@users.noreply.github.com>
2023-08-22 19:17:19 -0700

    Leetcode #0001 Two Sum Solution

---
+++ 0001_two_sum.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def twoSum(self, nums, target):
+        self.nums = nums
+        self.target = target
+
+        index = []
+
+        for i in range(len(self.nums)):
+            for j in range(i + 1, len(self.nums)):
+                if self.nums[i] + self.nums[j] == self.target:
+                    index.extend([i,j])
+
+        return
+
+
+# ----- TEST CASE ----- #
+nums = [2,7,11,15]
+target = 9
+index = Solution()
+print(index.twoSum(nums, target))
